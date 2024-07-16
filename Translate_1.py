0fe7638ff8851a49b308301ad6309b16f396f1e5
Aleksei Kavko <108331145+Aleks2317@users.noreply.github.com>
2024-07-16 22:25:57 +0300

    add button del

--- Translate_1.py
+++ Translate_1.py
@@ -1,8 +1,8 @@
-import tkinter as tk
 import customtkinter
 import sqlite3
 from datetime import date
 from googletrans import Translator
+from CTkListbox import *
 
 
 def db_start():
@@ -23,50 +23,43 @@ def db_start():
     date_now TEXT)""")
 
 
-def delete_translate():
-    """Функция для удаления переведенных запросов"""
-    index = list_translate_display.curselection()
-    print(f"{index = }")
-
-
-def list_translates_display():
-    """ Метода для выборки данных из таблиуы 'translation' и отображения в  """
-    list_translate_display.delete('1.0', 'end')
-    cur.execute("SELECT id, text, translation, date_now FROM translate_list")
-
-    title = cur.fetchall()
-    for i, t, tr, d in [titl for titl in reversed(title)]:
-        list_translate_display.insert(customtkinter.END, f'{"*" * 10} id = {i}. {"Ввод"}{"*" * 10}\n\n'
-                                                         f'{t.strip()}\n\n'
-                                                         f'{"*" * 10}{"Перевод"}{"*" * 10}\n\n'
-                                                         f'{tr.strip()}\n\n'
-                                                         f'{"_" * 10}{"Дата: "}{d}{"_" * 10}\n\n')
-
-
-# 13 создадим функцию для перевода
 def translate():
-    """Функция для перевода"""
+    """Функция для перевода м сохранения в db"""
+
     for launguage, suffix in languages.items():
-        print(comboTwo.get())
         if comboTwo.get() == suffix:
             date_now = str(date.today())
             text = t_input.get('1.0', 'end')
             translation = translator.translate(text, dest=suffix)  # принимает текст и суффикс языка
-            print(f'{comboTwo.get() = }'
-                  f'\n{text = }'
-                  f'\n{date_now = }'
-                  f'\n{translation.text = }')
             t_output.delete('1.0', 'end')
             t_output.insert('0.0', translation.text)
             cur.execute("INSERT INTO translate_list (text, translation, date_now) VALUES (?,?,?)",
                         (text, translation.text.rstrip(), date_now))
             conn.commit()
-            list_translates_display()
+            list_display()
 
 
+def delete_translate():
+    """Функция для удаления переведенных запросов"""
+    index = list_translate.curselection()
+    if index:
+        selected_list_display = list_translate.get(index[0])
+        cur.execute("DELETE FROM translate_list WHERE id=?", (selected_list_display[0],))
+        list_display()
 
 
-# 1 окно
+def list_display():
+    """ Метода для обнавления данных из таблиуы 'translation' и отображения в list_translate"""
+    list_translate.delete(0, customtkinter.END)
+    cur.execute("SELECT * FROM translate_list")
+    title = cur.fetchall()
+    for t in reversed(title):
+        list_translate.insert(customtkinter.END, (t[0], f"Текст: {t[1]}\n"
+                                                        f"Перевод: {t[2]}\n"
+                                                        f"Дата: {t[3]}\n"
+                                                        f"{'=' * 30}\n"))
+
+
 app = customtkinter.CTk()
 customtkinter.set_appearance_mode("dark")
 app.geometry('500x450')
@@ -74,19 +67,15 @@ app.title('Translate_1')
 app.resizable(width=False, height=False)
 translator = Translator()
 
-# 2 создадим словарь с языками
 languages = {'Русский': 'ru', 'English': 'en'}  # можно и другие языки добавлять
 
-# 3 создаем рамку
 header_frame = customtkinter.CTkFrame(app, )
 header_frame.pack()  # fill=x - растяжение рамки по X
 
-# 4 создадим внутри рамки столбцы
 header_frame.grid_columnconfigure(0, weight=1)
 header_frame.grid_columnconfigure(1, weight=1)
 header_frame.grid_columnconfigure(2, weight=1)
 
-# 5 создание виджета comboOne для отображения языков первого значения
 comboOne = customtkinter.CTkComboBox(header_frame,
                                      font=('Caveat', 20),
                                      values=[lang for lang in languages.values()],
@@ -96,7 +85,6 @@ comboOne = customtkinter.CTkComboBox(header_frame,
 comboOne.set(languages['Русский'])   # curent(0) по умолчанию вывод элемента с индексом 0
 comboOne.grid(row=0, column=0, pady=5, padx=5)  # вывод виджета в нулевой строке в нулевом столбце
 
-# 6 создание кнопки при нажатии на которую будет производиться перевод
 btn = customtkinter.CTkButton(header_frame,
                               text='Перевести',
                               font=('Caveat', 20),
@@ -106,63 +94,55 @@ btn = customtkinter.CTkButton(header_frame,
                               )
 btn.grid(row=0, column=1, pady=5, padx=5)
 
-# 7 создание виджета comboOne для отображения языков второго значения
 comboTwo = customtkinter.CTkComboBox(header_frame,
                                      font=('Caveat', 20),
                                      values=[lang for lang in languages.values()],
                                      state='readonly',
                                      width=165
                                      )  # state=readonly запрещает изменять размеры в виджите
-comboTwo.set(languages['English'])  # curent(1) по умолчанию вывод элемента с индексом 1
-comboTwo.grid(row=0, column=2, pady=5, padx=5)  # вывод виджета в нулевой строке втором столбце
+comboTwo.set(languages['English'])
+comboTwo.grid(row=0, column=2, pady=5, padx=5)
 
-# 8 рамка с полями с текстом
 translate_frame = customtkinter.CTkFrame(app)
 translate_frame.pack()
 
-# 9 разделяем поля на два столбца
 translate_frame.grid_columnconfigure(0, weight=1)
 translate_frame.grid_columnconfigure(1, weight=1)
 
-# 10 создание текстового поля для вводимого текста
 t_input = customtkinter.CTkTextbox(translate_frame,
                                    font=('Caveat', 15),
                                    width=245
                                    )
 t_input.grid(row=0, column=0, pady=5, padx=5)
 
-# 11 создание второго текстового поля для отображения перевода
 t_output = customtkinter.CTkTextbox(translate_frame,
                                     font=('Caveat', 15),
                                     width=245
                                     )
 t_output.grid(row=0, column=1, pady=5, padx=5)
 
-
-# добавим рамку для вывода прошлых запросов с возможностью удаления выбранной строки
 db_frame = customtkinter.CTkFrame(app, )
 db_frame.pack()
 
-# 12 текстовое поле для отображения последних переводов только для чтения
-list_translate_display = customtkinter.CTkTextbox(db_frame,
-                                                  font=('Caveat', 15),
-                                                  width=490,
-                                                  height=130,
-                                                  activate_scrollbars=True,  # активация прокрутки
-                                                  )
-list_translate_display.grid(row=0, column=0, padx=5, pady=5)
+list_translate = CTkListbox(db_frame,
+                            font=('Caveat', 15),
+                            width=450,
+                            height=130,
+                            multiple_selection=True,
+                            )
+list_translate.grid(row=0, column=0, padx=5, pady=5)
 
 db_del_frame = customtkinter.CTkFrame(app, )
 db_del_frame.pack()
 
-# внизу текстового поля предыдущих запросов создадим лейбл и кнопку удалить
 db_del_frame.grid_columnconfigure(0, weight=1)
 db_del_frame.grid_columnconfigure(1, weight=1)
 
-del_label = customtkinter.CTkLabel(db_del_frame,
-                                   font=('Caveat', 15),
-                                   width=350)
-del_label.grid(row=0, column=1, pady=5)
+# del_label = customtkinter.CTkLabel(db_del_frame,
+#                                    text='None',
+#                                    font=('Caveat', 15),
+#                                    width=350)
+# del_label.grid(row=0, column=1, pady=5)
 
 del_btn = customtkinter.CTkButton(db_del_frame,
                                   text='Удалить',
@@ -173,6 +153,9 @@ del_btn = customtkinter.CTkButton(db_del_frame,
 del_btn.grid(row=0, column=0, pady=1)
 
 db_start()
-list_translates_display()
+
+list_display()
+
 app.mainloop()
+
 conn.close()
